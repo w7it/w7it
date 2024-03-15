58f5f1ffba7f0972b185f7c6dc2b94ffd053269a
Vlad Orlov <vlad@w7it.com>
2024-03-15 13:49:29 +0200

    don't use route53 as dns

--- infra/route53.tf
+++ infra/route53.tf
@@ -8,8 +8,3 @@ resource "aws_route53domains_registered_domain" "w7it_com" {
     name = cloudflare_zone.base.name_servers[1]
   }
 }
-
-data "aws_route53_zone" "w7it_com" {
-  name         = "${local.base_domain}."
-  private_zone = false
-}
